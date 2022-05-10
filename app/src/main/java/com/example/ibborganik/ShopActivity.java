2431d1ab1adcbe13afed49c19c91e37422d300e2
Ere <edc@hotmail.com>
2022-05-10 23:22:13 +0300

    API dan Json string olarak çekildi

--- app/src/main/java/com/example/ibborganik/ShopActivity.java
+++ app/src/main/java/com/example/ibborganik/ShopActivity.java
@@ -6,6 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ShopActivity extends AppCompatActivity {
 
     @Override
@@ -23,12 +34,37 @@ public class ShopActivity extends AppCompatActivity {
         findViewById(R.id.button4).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                getVegetables(true);
             }
         });
     }
-    public static void getVegetables(){
+    public void getVegetables(boolean key){
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://halfiyatlaripublicdata.ibb.gov.tr/api/HalManager/getProductTypes";
+        // Request a string response from the provided URL.
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Display the first 500 characters of the response string.
+                        //alma yeri
+                        System.out.println(response.toString());
+                        try {
+                            System.out.println(response.get("Results"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("That didn't work!");
+                }
+        });
 
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
 }
