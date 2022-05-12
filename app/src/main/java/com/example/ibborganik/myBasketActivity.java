7cd0edbb757a8a75d597234d20c437a929a20f5c
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2022-05-12 14:41:33 +0300

    meyve ve sebze resimleri eklendi

---
+++ app/src/main/java/com/example/ibborganik/myBasketActivity.java
@@ -0,0 +1,14 @@
+package com.example.ibborganik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myBasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_basket);
+    }
+}
