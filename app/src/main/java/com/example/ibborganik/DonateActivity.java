d0ed4c69e3e38250ead8f95910b9712ef9d2bb64
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2022-05-13 03:10:25 +0300

    son

--- app/src/main/java/com/example/ibborganik/DonateActivity.java
+++ app/src/main/java/com/example/ibborganik/DonateActivity.java
@@ -2,7 +2,9 @@ package com.example.ibborganik;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class DonateActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class DonateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate);
+
+        findViewById(R.id.tavsiyeButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DonateActivity.this, TavsiyelerActivity.class));
+            }
+        });
     }
 }
